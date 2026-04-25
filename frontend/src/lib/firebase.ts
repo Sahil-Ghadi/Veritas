e19123c8eb58caa4ecf8626d39469999449a141d
Shridhar Mandrekar <geekyshridhar04@gmail.com>
2026-04-25 15:44:17 +0530

    env fixed

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2NiGZzswRM3G-fN1vtpvV_xtqVqmdBcs",
-  authDomain: "veritassssss.firebaseapp.com",
-  projectId: "veritassssss",
-  storageBucket: "veritassssss.firebasestorage.app",
-  messagingSenderId: "862742151502",
-  appId: "1:862742151502:web:113bce89f4113e5ee2de1f",
-  measurementId: "G-1CXXKL3ZT5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
